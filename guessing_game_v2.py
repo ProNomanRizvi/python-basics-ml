9e306989046ed62d9298ccb2237aca8c57a4b487
ProNomanRizvi <pro.noman.rizvi@gmail.com>
2026-03-22 11:57:16 +0500

    Day 2: Loops and problem solving

--- guessing_game_v2.py
+++ guessing_game_v2.py
@@ -2,6 +2,8 @@ secert = 7
 
 guess = int(input("Enter a number: "))
 
+count = 0
+
 if guess < 0:
     print("Please enter a positive number")
     exit()
@@ -9,8 +11,17 @@ if guess < 0:
 while guess != secert:
     if guess < secert:
         print("Too low")
+        count += 1
+        if count == 3:
+            print("You have exceeded the number of attempts")
+            exit()
     else:
         print("Too high")
+        count += 1
+        if count == 3:
+            print("You have exceeded the number of attempts")
+            exit()
     guess = int(input("Enter a number: "))
 
 print("Correct")
+print(f"You guessed the number in {count} attempts")
